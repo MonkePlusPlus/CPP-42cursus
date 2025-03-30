ec47b786baff2b63200570ac511b56f4162b26f3
Polgar Theo <ptheo@bess-f2r1s19.clusters.42paris.fr>
2025-03-30 19:27:34 +0200

    C07 finish

--- C07/ex00/src/main.cpp
+++ C07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ptheo <ptheo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 22:01:25 by ptheo             #+#    #+#             */
-/*   Updated: 2025/03/29 22:05:14 by ptheo            ###   ########.fr       */
+/*   Updated: 2025/03/30 19:26:41 by ptheo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ int main( void ) {
 	int a = 2;
 	int b = 3;
 	
-	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap( a, b );
+	std::cout << "swap : a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	
--- C07/ex02/src/main.cpp
+++ C07/ex02/src/main.cpp
@@ -5,13 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ptheo <ptheo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/29 22:19:41 by ptheo             #+#    #+#             */
-/*   Updated: 2025/03/29 22:48:43 by ptheo            ###   ########.fr       */
+/*   Created: 2025/03/30 19:20:35 by ptheo             #+#    #+#             */
+/*   Updated: 2025/03/30 19:23:46 by ptheo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Array.hpp"
+#include <iostream>
+#include <Array.hpp>
+#include <stdlib.h>
 
-int main() {
-	return (0);
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+	Array<int> tmp = numbers;
+	Array<int> test(tmp);
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i] || tmp[i] != numbers[i] || numbers[i] != test[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
